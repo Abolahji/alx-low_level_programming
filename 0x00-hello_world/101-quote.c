b5573ccd073a2b5365b01d2cf258a2f284ea5f22
Abolahji <abolahji@gmail.com>
2023-03-17 15:59:59 +0000

    cisfun

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - is the running point
+ *
+ * Return: will be one here
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return 1;
+}
