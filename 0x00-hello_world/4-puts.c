9252aab70e2a3a09108a85aa660ed9828aef7d99
Abolahji <abolahji@gmail.com>
2023-03-17 02:41:23 +0000

    cisfun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is my starting point 
+ * 
+ * Return: i do not have any codes left to be executed
+ */
+
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
