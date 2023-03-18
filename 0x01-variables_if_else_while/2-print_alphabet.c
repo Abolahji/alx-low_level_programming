a7a8385705ac4fc78f17ed0c48ceae9dfa2ce458
Abolahji <abolahji@gmail.com>
2023-03-18 00:00:34 +0000

    cisfun

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - the starting point
+ *
+ * Return: still zero
+ */
+int main(void)
+{
+char alx;
+for (alx = 'a' ; alx <= 'z' ; alx++)
+putchar(alx);
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - void for now
+ *
+ * Return: zero
+ */
+int main(void)
+{
+char alx;
+for (alx = 'a' ; alx <= 'z' ; alx++)
+putchar(alx);
+for (alx = 'A' ; alx <= 'Z' ; alx++)
+putchar(alx);
+putchar('\n');
+return (0);
+}
