ec2aadb718d54aaf9e4538c16bcce4f70badea87
Abolahji <abolahji@gmail.com>
2023-03-18 02:48:28 +0000

    cisfun

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ *
+ * Return: zero
+ */
+int main(void)
+{
+int alx;
+char course;
+for (alx = 0 ; alx < 10 ; alx++);
+putchar(alx + '0');
+for (course = 'a' ; course <= 'f' ; course++)
+putchar(course);
+putchar('\n');
+return (0);
+}
