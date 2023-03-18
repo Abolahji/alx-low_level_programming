993dd47a7b91c2c46efd05807df5ef39329f0914
Abolahji <abolahji@gmail.com>
2023-03-18 02:13:44 +0000

    cisfun

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+/**
+ * main - starting point
+ *
+ * Return: zero
+ */
+int main(void)
+{
+int alx;
+for (alx = 0 ; alx < 10 ; alx++)
+putchar(alx + '0');
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+/**
+ * main - essential
+ *
+ * Return: zero
+ */
+int main(void)
+{
+char alx;
+for (alx = 'z' ; alx >= 'a' ; alx--)
+putchar(alx);
+putchar('\n');
+return (0);
+}
