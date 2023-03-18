c55f2bdd248a05fdf1fbdfe217e2311653917cb6
Abolahji <abolahji@gmail.com>
2023-03-18 00:50:48 +0000

    cisfun

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - specified again
+ *
+ * Return: zero gang
+ */
+int main(void)
+{
+int a;
+for (a = '0' ; a < 10 ; a++)
+putchar('a');
+putchar('\n');
+return (0);
+}
