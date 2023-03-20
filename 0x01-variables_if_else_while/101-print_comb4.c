5d712cf2295ff8d82e620227d501eb204f4f4975
Abolahji <abolahji@gmail.com>
2023-03-20 21:20:13 +0000

    cisfun

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,12 +9,27 @@ int main(void)
 int cis;
 int really;
 int fun;
-{
 for (cis = 0 ; cis < 10 ; cis++)
 {
-for (really = 0 ; really < 10 ; really++)
+for (really = 1 ; really < 10 ; really++)
 {
-for (fun = 0 ; fun < 10 ; fun++)
+for (fun = 2 ; fun < 10 ; fun++)
 {
-if (cis < really && cis < fun && cis != really && cis != fun)
-	putv
+if (cis < really && cis < fun && cis != really && cis != fun
+&& really < fun && really != fun)
+{
+putchar(cis + '0');
+putchar(really + '0');
+putchar(fun + '0');
+if (cis + really + fun < 24)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
