c40a2eab8c27eebb12ab869320133de8cfcaa17d
Abolahji <abolahji@gmail.com>
2023-03-22 22:44:14 +0000

    nested

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - my function
+ * Return: zero
+ */
+void print_alphabet(void)
+{
+char nested;
+for (nested = 'a' ;  nested <= 'z' ; nested++)
+_putchar(nested);
+_putchar('\n');
+}
