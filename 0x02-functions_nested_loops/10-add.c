beb4f649de121f6bbeabb4c492a8e80b6ff7c7a3
Abolahji <abolahji@gmail.com>
2023-03-24 03:59:04 +0000

    nested

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add -  a function that adds two integers and returns the result.
+ * @iftar: a parameter
+ * @sahur: a parameter
+ * Return: is the sum of variables
+ */
+int add(int iftar, int sahur)
+{
+iftar = 48;
+sahur = 50;
+return (iftar + sahur);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ * times_table -  a function that prints the 9 times table, starting with 0
+ * Return: void
+ */
+void times_table(void)
+{  
